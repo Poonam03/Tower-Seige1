715178ac0d1ac4777645db45e67be499e488972d
Poonam03 <68641541+Poonam03@users.noreply.github.com>
2020-12-16 23:43:56 +0530

    Update box.js

--- box.js
+++ box.js
@@ -5,7 +5,7 @@ class Box
         var options={
             isStatic:false,
             restitution:0.8,
-            friction:0.5           
+            friction:0.0           
         }
         this.body=Bodies.rectangle(x,y,width,height,options);
         this.width=width;
@@ -17,9 +17,7 @@ class Box
         push();
         translate(this.body.position.x,this.body.position.y)
         rotate(this.body.angle)
-        stroke(10);
-        fill("green")
-        rectMode(CENTER);
+         rectMode(CENTER);
         rect(0,0,this.width,this.height)
         pop()
     }
